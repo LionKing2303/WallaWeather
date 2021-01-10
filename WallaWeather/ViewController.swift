dd479cf7789ca0f0075ce0053943c0a83be06c9c
Arie Peretz <ariep20@gmail.com>
2021-01-10 19:10:53 +0200

    Initial Commit

---
+++ WallaWeather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WallaWeather
+//
+//  Created by Arie Peretz on 10/01/2021.
+//  Copyright © 2021 Arie Peretz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
